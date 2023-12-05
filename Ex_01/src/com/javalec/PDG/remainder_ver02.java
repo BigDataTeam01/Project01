96527612f47985893b4d466899bcd005cc28ec7c
ForrestDPark <pulpilisory@gmail.com>
2023-12-05 14:42:11 +0900

    pdg

---
+++ Ex_01/src/com/javalec/PDG/remainder_ver02.java
@@ -0,0 +1,29 @@
+package com.javalec.PDG;
+
+public class remainder {
+	
+	
+	
+	// Filed
+	int [] result = new int[2];
+	
+	//Constructor
+	
+	
+	
+	// Method
+	
+	
+	public int[] remainder (int num1 , int num2 ) {
+		
+		result[0] =num1%num2;
+		result[1] = num1%num2;
+		
+		
+		return result;
+	}
+	
+	
+	
+
+}
