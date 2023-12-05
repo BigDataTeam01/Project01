8c9a1384e6f17c337d6d43559eb95d1fa53b8b96
zlzlwn <83982135+zlzlwn@users.noreply.github.com>
2023-12-05 14:26:23 +0900

    first1
    1번

--- Ex_01/src/com/javalec/base/Main.java
+++ Ex_01/src/com/javalec/base/Main.java
@@ -1,10 +1,22 @@
 package com.javalec.base;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 숫자 3개 받기
+		Scanner scanner = new Scanner(System.in);
+		
+		int num1 =0;
+		int num2 =0;
+		int num3 =0;
+		
+		num1 =scanner.nextInt();
+		num2 =scanner.nextInt();
+		num3 =scanner.nextInt();
+		
+		
 	}
 
 }
